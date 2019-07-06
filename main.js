16df6c836965c7d7058d8fe1bdc2e715f1171a9a
unknown <aravindm@AJRDR013.ajrdc.lan>
2019-07-06 14:42:39 +0530

    Revert "changes in dev"
    This reverts commit 8675fc6f75b1bfdf3d92c66c9443c2a0a62a49ef.
    
    reverted

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // Changes in dev branch
-// another change in dev branch
 const { app, BrowserWindow, Menu, MenuItem } = require('electron');
 // const notifier = require('node-notifier');
 const path = require("path");
