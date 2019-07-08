df11b6abf4d13f3dcb478cb6827622697d511ed5
Aravind <aravind.merugu@ajrinfosystems.com>
2019-07-08 17:35:36 +0530

    commit to dev and master

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// merge from master branch
+// some changes and commit to dev and master
 const { app, BrowserWindow, Menu, MenuItem } = require('electron');
 // const notifier = require('node-notifier');
 const path = require("path");
